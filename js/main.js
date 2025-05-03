7a908a688bda01723e284424b6fd72ff915a9ef5
KwolekD <damiankwolek1@gmail.com>
2025-05-03 17:56:42 +0200

    before deploying

--- js/main.js
+++ js/main.js
@@ -6,3 +6,23 @@ window.onload = () => {
             .catch((error) => console.error('Service Worker registration failed:', error));
     }
 };
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const hamburger = document.createElement('div');
+//     hamburger.className = 'hamburger';
+//     hamburger.innerHTML = `
+//         <div class="line"></div>
+//         <div class="line"></div>
+//         <div class="line"></div>
+//     `;
+
+//     const container = document.querySelector('nav .container');
+//     container.appendChild(hamburger);
+
+//     const navLinks = document.querySelector('.nav-links');
+
+//     hamburger.addEventListener('click', () => {
+//         hamburger.classList.toggle('active');
+//         navLinks.classList.toggle('active');
+//     });
+// });
